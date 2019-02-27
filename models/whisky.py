366a5321a12c9b36716f385566c7a6c9d6d48c29
Ben Lander <ben.l.lander@googlemail.com>
2019-02-27 08:35:55 +0000

    Whisky model added

--- models/whisky.py
+++ models/whisky.py
@@ -7,3 +7,10 @@ class Whisky(db.Model, BaseModel):
 
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Int, nullable=True)
+    description = db.Column(db.String(500), nullable=False)
+    abv = db.Column(db.Float, nullable=False)
+    cask = db.Column(db.String(30), nullable=True)
+
+class WhiskySchema(ma.ModelSchema, BaseSchema):
+    class Meta:
+        model = Whisky
