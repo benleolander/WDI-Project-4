74c17b2fc908b6d4851003b9031f244431a64e0b
Ben Lander <ben.l.lander@googlemail.com>
2019-03-05 09:41:04 +0000

    Added initial password & password_confirmation error handling

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -13,7 +13,8 @@ class Register extends React.Component {
         email: '',
         password: '',
         password_confirmation: ''
-      }
+      },
+      errors: {}
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -21,7 +22,8 @@ class Register extends React.Component {
 
   handleChange({target: { name, value }}) {
     const data = {...this.state.data, [name]: value }
-    this.setState({ data })
+    const errors = {...this.state.errors, [name]: null}
+    this.setState({ data, errors })
   }
 
 
@@ -30,14 +32,17 @@ class Register extends React.Component {
     axios
       .post('/api/register', this.state.data)
       .then(() => this.props.history.push('/login'))
+      .catch(err => this.setState({ errors: err.response.data}))
   }
 
   render() {
+    const errors = this.state.errors
     return(
       <section className="section">
         <div className="container">
           <RegisterForm
             data={this.state.data}
+            errors={errors}
             handleChange={this.handleChange}
             handleSubmit={this.handleSubmit}
           />
